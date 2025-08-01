4f055a550e80f9429fec3f6297cca9e4352d77c8
mfernandail <maria.ibanez.leiva@gmail.com>
2025-07-31 21:33:31 -0400

    Refactor currency fetching and rendering logic

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
 const dropCurrency = document.getElementById('currency')
+const result = document.getElementById('result')
 
-document.addEventListener('DOMContentLoaded', fetchCurrency)
+document.addEventListener('DOMContentLoaded', function () {
+  const url = 'https://api.frankfurter.dev/v1/currencies'
+  fetchCurrency(url, 1)
+})
 
-async function fetchCurrency() {
+dropCurrency.addEventListener('change', searchCurrency)
+
+function searchCurrency(e) {
+  const currencySelected = e.target.value
+
+  const url = `https://api.frankfurter.dev/v1/latest?base=${currencySelected}`
+
+  console.log(currencySelected)
+
+  fetchCurrency(url, 2)
+}
+
+async function fetchCurrency(url, typeEndpoint) {
   try {
-    const url = 'https://api.frankfurter.dev/v1/currencies'
     const response = await fetch(url)
 
     if (!response.ok) throw new Error('Error to get data')
 
     const data = await response.json()
 
-    Object.keys(data).forEach((currencyCode) => {
-      const op = document.createElement('option')
-
-      op.value = currencyCode
-      op.textContent = currencyCode
-
-      dropCurrency.appendChild(op)
-    })
+    typeEndpoint === 1 ? renderSelect(data) : renderResult(data)
   } catch (error) {
     console.log('Error: ', error)
   }
+}
+
+function renderSelect(data) {
+  Object.keys(data).forEach((currencyCode) => {
+    const op = document.createElement('option')
+
+    op.value = currencyCode
+    op.textContent = currencyCode
+
+    dropCurrency.appendChild(op)
+  })
+}
+
+function renderResult(data) {
+  result.innerHTML = ''
+  const { date, rates, base } = data
+
+  const dateP = document.createElement('p')
+  const baseP = document.createElement('p')
+  const table = document.createElement('tr')
+
+  dateP.textContent = date
+  baseP.textContent = base
+
+  Object.entries(rates).forEach((currency) => {
+    const row = document.createElement('tr')
+    row.innerHTML = `
+      <td>${currency[0]}</td>
+      <td>${currency[1]}</td>
+    `
+    table.appendChild(row)
+  })
+
+  result.appendChild(dateP)
+  result.appendChild(baseP)
+  result.appendChild(table)
 }
